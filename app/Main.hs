42b2e733cdfddd229c2d7a0aef37c9cb2ad870df
Robb Shecter <robb@weblaws.org>
2017-01-26 21:41:41 -0800

    added a bunch of code in

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
-module Main where
+import           Text.HandsomeSoup
+import           Text.XML.HXT.Core
 
-import Lib
 
-main :: IO ()
-main = someFunc
+data Title = Title {
+  header :: Char,
+  body   :: [Char]
+} deriving (Show)
+
+main = do
+  html <- readFile "nrs.html"
+  print (foldl addRawData [] ['A', 'a', 'b', 'c', 'd', 'B', '1', '2', '3'])
+
+
+addRawData :: [Title] -> Char -> [Title]
+addRawData titles aLine =
+  if isHeader aLine
+    then Title {header=aLine, body=[]} : titles
+    else Title {header=(header . head) titles, body=(body . head) titles ++ [aLine] } : tail titles
+
+
+isHeader :: Char -> Bool
+isHeader ch =
+  ch == 'A' || ch == 'B'
