1d94945a851d7eb964c1f96a341e5e72a4feb963
Robb Shecter <robb@weblaws.org>
2017-01-28 04:58:28 -0800

    more pieces

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,29 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+import           Data.Aeson               (ToJSON)
+import           Data.Aeson.Encode.Pretty (encodePretty)
+import qualified Data.ByteString.Lazy     as B
+import           GHC.Generics
 import           Text.HandsomeSoup
 import           Text.XML.HXT.Core
 
 
-data Title = Title {
-  header :: Int,
-  body   :: [Int]
-} deriving (Show)
+data Title =
+  Title {
+    header :: Int,
+    body   :: [Int]
+  } deriving (Generic, Show)
+
+instance ToJSON Title
 
 
 main = do
   html <- readFile "nrs.html"
-  print (foldl addItem [] [0, 1, 2, 3, 4, 5, 0, 9, 9, 9])
+  let doc = readString [withParseHTML yes, withWarnings no] html
+  items <- runX $ doc >>> css "td"
+  let things = foldl addItem [] [0, 1, 2, 3, 4, 5, 0, 9, 9, 9]
+  B.putStr (encodePretty things)
 
 
 addItem :: [Title] -> Int -> [Title]
