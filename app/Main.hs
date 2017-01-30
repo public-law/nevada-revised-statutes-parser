b8f0755081fddf2a04230190e90493a769729d42
Robb Shecter <robb@weblaws.org>
2017-01-29 20:27:39 -0800

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -9,13 +9,10 @@ import           Text.HandsomeSoup
 import           Text.XML.HXT.Core
 
 
-class Treeable a where
-  isHeader :: a -> Bool
-
 data Title =
   Title {
-    header :: Int,
-    body   :: [Int]
+    header :: Integer,
+    body   :: [Integer]
   } deriving (Generic, Show)
 
 instance ToJSON Title
@@ -25,16 +22,21 @@ main = do
   html <- readFile "nrs.html"
   let doc = readString [withParseHTML yes, withWarnings no] html
   items <- runX $ doc >>> css "td"
-  let things = foldl addItem [] [0, 1, 2, 3, 4, 5, 0, 9, 9, 9]
-  B.putStr (encodePretty things)
+  let things = shallowTree [0, 1, 2, 3, 4, 5, 0, 9, 9, 9]
+  B.putStr (encodePretty $ reverse things)
 
 
-addItem :: [Title] -> Int -> [Title]
+shallowTree :: [Integer] -> [Title]
+shallowTree items =
+  reverse $ foldl addItem [] items
+
+
+addItem :: [Title] -> Integer -> [Title]
 addItem things item =
   if isHeader item
     then Title {header=item, body=[]} : things
     else Title {header=(header . head) things, body=(body . head) things ++ [item] } : tail things
 
 
-instance Treeable Integer where
-  isHeader i  =   i == 0
+isHeader 0 = True
+isHeader _ = False
