f7792e72fc3079ce2ab3dc45fd29701cc804a5ea
Robb Shecter <robb@weblaws.org>
2017-02-05 02:35:35 -0800

    cleanup

--- src/NvStatutes.hs
+++ src/NvStatutes.hs
@@ -1,49 +1,50 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module NvStatutes where
-  import           Data.Aeson               (ToJSON)
-  import           Data.Aeson.Encode.Pretty (confCompare, defConfig,
-                                             encodePretty', keyOrder)
-  import qualified Data.ByteString.Lazy     as B
-  import           Data.Function            ((&))
-  import           Data.List.Split          (chunksOf, split, whenElt)
-  import           Text.HTML.TagSoup        (parseTags, partitions, (~/=), (~==))
-  import           GHC.Generics
 
-  data Title =
-    Title {
-      titleName :: String,
-      chapters  :: [Chapter]
-  } deriving (Generic, Show)
+import           Data.Aeson               (ToJSON)
+import           Data.Aeson.Encode.Pretty (confCompare, defConfig,
+                                           encodePretty', keyOrder)
+import qualified Data.ByteString.Lazy     as B
+import           Data.Function            ((&))
+import           Data.List.Split          (chunksOf, split, whenElt)
+import           GHC.Generics
+import           Text.HTML.TagSoup        (parseTags, partitions, (~/=), (~==))
 
-
-  data Chapter =
-    Chapter {
-      chapterName :: String,
-      number      :: String,
-      url         :: String
-  } deriving (Generic, Show)
-
-  instance ToJSON Title
-  instance ToJSON Chapter
-
-  titleCount indexHtml =
-    let tags   = parseTags indexHtml
-        table  = head $ partitions (~== "<table class=MsoNormalTable") tags
-        rows   = partitions (~== "<tr>") table
-        tuples = rowTuples rows
-    in length tuples
+data Title =
+  Title {
+    titleName :: String,
+    chapters  :: [Chapter]
+} deriving (Generic, Show)
 
 
-  rowTuples rows =
-    split (whenElt isTitleRow) rows
-      & tail
-      & chunksOf 2
+data Chapter =
+  Chapter {
+    chapterName :: String,
+    number      :: String,
+    url         :: String
+} deriving (Generic, Show)
+
+instance ToJSON Title
+instance ToJSON Chapter
+
+titleCount indexHtml =
+  let tags       = parseTags indexHtml
+      table      = head $ partitions (~== "<table class=MsoNormalTable") tags
+      rows       = partitions (~== "<tr>") table
+      title_rows = filter isTitleRow rows
+  in length title_rows
 
 
-  isTitleRow r =
-    length (partitions (~== "<td>") r) == 1
+rowTuples rows =
+  split (whenElt isTitleRow) rows
+    & tail
+    & chunksOf 2
 
 
-  newChapter row =
-    Chapter {chapterName="", number="", url=""}
+isTitleRow r =
+  length (partitions (~== "<td>") r) == 1
+
+
+newChapter row =
+  Chapter {chapterName="", number="", url=""}
--- test/MiscSpec.hs
+++ test/MiscSpec.hs
@@ -1,15 +1,13 @@
 module MiscSpec where
 
-  import           Test.Hspec
-  import   NvStatutes
+import           NvStatutes
+import           Test.Hspec
 
+main :: IO ()
+main = hspec spec
 
-  main :: IO()
-  main = hspec spec
-
-  spec :: Spec
-  spec =
-    describe "titleCount" $
-      it "finds the correct number of titles" $ do
+spec :: Spec
+spec = describe "titleCount" $
+    it "finds the correct number of titles" $ do
         html <- readFile "nrs.html"
         titleCount html `shouldBe` 59
