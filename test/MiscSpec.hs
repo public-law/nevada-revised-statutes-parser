9b3da0d071017670c5a0959dae9a401dfb37b256
Robb Shecter <robb@weblaws.org>
2017-02-07 19:02:55 -0800

    failing test

--- test/MiscSpec.hs
+++ test/MiscSpec.hs
@@ -4,10 +4,17 @@ import           NvStatutes
 import           Test.Hspec
 
 main :: IO ()
-main = hspec spec
-
-spec :: Spec
-spec = describe "titleCount" $
+main = hspec $ do
+  describe "titleCount" $
     it "finds the correct number of titles" $ do
-        html <- readFile "nrs.html"
-        titleCount html `shouldBe` 59
+      html <- readFile "nrs.html"
+      titleCount html `shouldBe` 59
+
+  describe "titles" $ do
+    it "gets the first title's number " $ do
+      html <- readFile "nrs.html"
+      title_number (head (titles html)) `shouldBe` 1
+
+    it "gets the first title's name" $ do
+      html <- readFile "nrs.html"
+      title_name (head (titles html)) `shouldBe` "STATE JUDICIAL DEPARTMENT"
