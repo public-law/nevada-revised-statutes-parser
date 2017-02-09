44afb5f7ff1887931f303efecac9d79d28589d42
Robb Shecter <robb@weblaws.org>
2017-02-09 00:43:32 -0800

    first pass

--- src/Models.hs
+++ src/Models.hs
@@ -4,8 +4,22 @@ module Models where
 
 import           Data.Aeson   (ToJSON)
 import           Data.Text    (Text)
+import           Data.Time    (Day, defaultTimeLocale, parseTimeOrError)
 import           GHC.Generics (Generic)
 
+
+data NRS =
+  NRS {
+  statuteTree  :: Tree,
+  nominalDate  :: Year,
+  dateAccessed :: Day
+} deriving (Generic, Show)
+
+data Tree =
+  Tree {
+  chapter0   :: Chapter,
+  treeTitles :: [Title]
+} deriving (Generic, Show)
 
 data Title =
   Title {
@@ -18,9 +32,21 @@ data Chapter =
   Chapter {
     chapterName   :: Text,
     chapterNumber :: Text,
+    sections      :: [Section],
     url           :: Text
 } deriving (Generic, Show)
 
+data Section =
+  Section {
+    sectionName   :: Text,
+    sectionNumber :: Text,
+    sectionBody   :: Text
+} deriving (Generic, Show)
 
+instance ToJSON NRS
+instance ToJSON Tree
 instance ToJSON Title
 instance ToJSON Chapter
+instance ToJSON Section
+
+type Year = Integer
