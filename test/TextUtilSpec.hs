bb62e22872ffa318f6cfff27eef98212f5de6f5d
Robb Shecter <robb@weblaws.org>
2017-09-16 21:15:36 -0700

    Final test for titleizeWord

--- test/TextUtilSpec.hs
+++ test/TextUtilSpec.hs
@@ -11,6 +11,18 @@ import           TextUtil       (titleize, titleizeWord)
 spec :: SpecWith ()
 spec = parallel $ do
 
+  describe "titleizeWord" $ do
+    
+        it "handles a single punctuation character prefix" $
+          titleizeWord "!snurk" `shouldBe` "!Snurk"
+    
+        it "handles a word with no punctuation" $
+          titleizeWord "snurk" `shouldBe` "Snurk"
+    
+        it "handles a word in quotes" $
+          titleizeWord "“ABUSE”" `shouldBe` "“Abuse”"
+
+
   describe "titleize" $ do
 
     it "handles all uppercase" $ do
@@ -22,13 +34,3 @@ spec = parallel $ do
     it "handles Unicode double quotes" $ do
       pending
       titleize "“ABUSE OR NEGLECT” DEFINED." `shouldBe` "“Abuse or Neglect” Defined."
-
-
-  describe "titleizeWord" $ do
-
-    it "handles a single punctuation character prefix" $
-      titleizeWord "!snurk" `shouldBe` "!Snurk"
-
-    it "handles a word with no punctuation" $
-      titleizeWord "snurk" `shouldBe` "Snurk"
-    
