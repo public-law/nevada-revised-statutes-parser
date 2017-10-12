22946618bfab06c193c5438e2bfd717c37f415dc
Robb Shecter <robb@weblaws.org>
2017-10-11 18:05:33 -0700

    sourceDir cmd line arg

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax     #-}
 
 import           BasicPrelude
+import           ChapterFile
 import           Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy     as B
 import           Data.Eq.Unicode
-
-import           ChapterFile
 import           FileUtil
+import           System.Environment       (getArgs)
 
 
 main :: IO ()
 main = do
-    args ← getArgs
+    args ← System.Environment.getArgs
     when (length args ≠ 1)
-      (fail "Usage: parse-nevada [directory]")
+        (fail "Usage: parse-nevada [filename]")
+
     let sourceDir = head args
 
     html ← readFileLatin1 (fixture "nrs-432b.html")
