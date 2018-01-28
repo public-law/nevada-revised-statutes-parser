ca3d8d215d4ccf0d0d65136bac35708c64754cae
Robb Shecter <robb@public.law>
2018-01-27 18:43:56 -0800

    feat: Parses all chapters

--- src/TreeParser.hs
+++ src/TreeParser.hs
@@ -8,21 +8,22 @@ import qualified Data.HashMap.Lazy as HM
 import           ChapterFile       (parseChapter)
 import           FileUtil
 import           HtmlUtil          (Html, toText)
+import           IndexFile         (parseTitles)
 import           Models.Chapter
 import           Models.Tree
 
 type ChapterMap = HashMap RelativePath Html
 
 parseTree :: Html -> ChapterMap -> Tree
-parseTree _indexFile chapterMap =
+parseTree indexFile chapterMap =
     Tree {
-        chapter0 = chapterZero chapterMap,
-        titles   = []
+        chapter0 = getChapterZero chapterMap,
+        titles   = parseTitles indexFile
     }
 
 
-chapterZero :: ChapterMap -> Chapter
-chapterZero chapterMap =
+getChapterZero :: ChapterMap -> Chapter
+getChapterZero chapterMap =
     case HM.lookup chapterZeroPathname chapterMap of
         Just html -> parseChapter $ toText html
         Nothing   -> error $ "Chapter Zero " ++ (show chapterZeroPathname) ++ " not found in " ++ (show $ head $ HM.keys chapterMap)
