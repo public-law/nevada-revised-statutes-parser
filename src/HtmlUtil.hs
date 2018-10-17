8da1ff17aff05b839fb5a29b88386c06a5fd6c1c
Robb Shecter <robb@public.law>
2018-10-17 02:40:24 -0700

    brittany formatting and hlinting

--- src/HtmlUtil.hs
+++ src/HtmlUtil.hs
@@ -1,47 +1,57 @@
-module HtmlUtil  where
+module HtmlUtil where
 
 import           BasicPrelude
-import qualified Data.Text as T
-import           Text.HTML.TagSoup (Tag, innerText, partitions, (~/=), (~==))
-import           GHC.Generics (Generic)
-import           Data.Aeson   (ToJSON)
+import qualified Data.Text                     as T
+import           Text.HTML.TagSoup              ( Tag
+                                                , innerText
+                                                , partitions
+                                                , (~/=)
+                                                , (~==)
+                                                )
+import           GHC.Generics                   ( Generic )
+import           Data.Aeson                     ( ToJSON )
 
-import           FileUtil          (AbsolutePath, readFileLatin1, toFilePath, fixture)
+import           FileUtil                       ( AbsolutePath
+                                                , readFileLatin1
+                                                , toFilePath
+                                                , fixture
+                                                )
 
 
-html_fixture :: FilePath -> IO Html
-html_fixture fname = do
+htmlFixture :: FilePath -> IO Html
+htmlFixture fname = do
     text <- readFileLatin1 (fixture fname)
     return $ makeHtml text
 
 
-
 readHtmlFile :: AbsolutePath -> IO Html
-readHtmlFile file = NewHtml <$> (readFileLatin1 $ toFilePath file)
+readHtmlFile file = NewHtml <$> readFileLatin1 (toFilePath file)
 
 
 -- Return the text content of an HTML title.
 titleText :: [Tag Text] -> Text
-titleText tags =
-    case findFirst "<title>" tags of
-        (_:y:_) -> innerText [y]
-        _       -> error $ "No <title> found in: " ++ (show tags)
+titleText tags = case findFirst "<title>" tags of
+    (_ : y : _) -> innerText [y]
+    _           -> error $ "No <title> found in: " ++ (show tags)
 
 
 -- Return the first occurrence of an HTML tag within the given
 -- HTML chunk.
 findFirst ∷ Text → [Tag Text] → [Tag Text]
-findFirst searchTerm tags =
-    case findAll searchTerm tags of
-        (x:_) -> x
-        _     -> error $ "Could not find the tag " ++ (T.unpack searchTerm) ++ " in: " ++ (show tags)
+findFirst searchTerm tags = case findAll searchTerm tags of
+    (x : _) -> x
+    _ ->
+        error
+            $  "Could not find the tag "
+            ++ T.unpack searchTerm
+            ++ " in: "
+            ++ show tags
 
 
 -- Return all the occurrences of an HTML tag within the given
 -- HTML chunk.
 findAll ∷ Text → [Tag Text] → [[Tag Text]]
-findAll searchTerm =
-    partitions (~== (T.unpack searchTerm))
+findAll searchTerm = partitions (~== T.unpack searchTerm)
 
 
 shaveBackTagsToLastClosingP :: [Tag Text] -> [Tag Text]
@@ -58,7 +68,7 @@ newtype Html = NewHtml Text
 instance ToJSON Html
 
 makeHtml :: Text -> Html
-makeHtml text = NewHtml text
+makeHtml = NewHtml
 
 toText :: Html -> Text
 toText (NewHtml t) = t
