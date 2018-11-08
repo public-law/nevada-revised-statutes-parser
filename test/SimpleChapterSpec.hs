966c0f9680c294cf4157d89ed5f5dff68673bcbb
Robb Shecter <robb@public.law>
2018-11-07 19:59:51 -0800

    next passing test

--- test/SimpleChapterSpec.hs
+++ test/SimpleChapterSpec.hs
@@ -37,7 +37,12 @@ spec = parallel $ describe "parseChapter" $ do
     let firstSection = head $ simpleChapterContent html
     Section.number firstSection `shouldBe` "0.010"
 
-  it "finds the correct section content" pending
+  it "finds the correct section content" $ do
+    html <- chapter_0_html
+    let firstSection = head $ simpleChapterContent html
+    Section.body firstSection
+      `shouldBe` "<p class=SectBody><span class=\"Section\">0.010</span><span class=\"Empty\"> </span><span class=\"Leadline\">Scope.</span><span class=\"Empty\"> </span>This chapter provides definitions and declarations of legislative intent which apply to Nevada Revised Statutes as a whole.</p> <p class=\"SourceNote\"> (Added to NRS by <a href=\"../Statutes/59th/Stats197701.html#Stats197701page181\">1977, 181</a>)</p>"
+
 
   it "finds the correct section names" $ do
     pendingWith "TODO: bug fix"
@@ -45,6 +50,9 @@ spec = parallel $ describe "parseChapter" $ do
     let lastSection = last $ simpleChapterContent html
     Section.name lastSection `shouldBe` "“Substantial bodily harm” defined."
 
+  it "finds the correct section number"  pending
+
+  it "finds the correct section content" pending
 
 --
 -- Helper Functions
