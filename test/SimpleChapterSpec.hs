5948a85b584faf6fce0750e21edd9e779988f7e7
Robb Shecter <robb@public.law>
2018-11-07 19:34:58 -0800

    refactor: introduce expectationFailure

--- test/SimpleChapterSpec.hs
+++ test/SimpleChapterSpec.hs
@@ -6,9 +6,6 @@ module SimpleChapterSpec where
 import           BasicPrelude
 import           Test.Hspec
 
--- What exactly is this?
--- import          Text.InterpolatedString.Perl6 (q)
-
 import           Models.Chapter                as Chapter
 
 import           ChapterFile
@@ -16,29 +13,25 @@ import           HtmlUtil
 
 
 spec :: SpecWith ()
-spec = parallel $
+spec = parallel $ describe "parseChapter" $ do
 
-  describe "parseChapter" $ do
+  it "recognizes a chapter with simple content" $ do
+    html ← chapter_0_html
+    case content (parseChapter html) of
+      SimpleChapterContent _ -> pure ()
+      ComplexChapterContent xs ->
+        expectationFailure $ "Expected Sections but got SubChapters" ++ show xs
 
-    it "recognizes a chapter with simple content" $ do
-      html ← chapter_0_html
-      case content (parseChapter html) of
-        SimpleChapterContent _ -> True `shouldBe` True
-        ComplexChapterContent _ -> False `shouldBe` True
+  it "finds the correct number of sections" $ do
+    html ← chapter_0_html
+    case content (parseChapter html) of
+      SimpleChapterContent sections -> length sections `shouldBe` 21
+      ComplexChapterContent xs ->
+        expectationFailure $ "Got Subchapters but expected Sections" ++ show xs
 
-    it "finds the correct number of sections" $ do
-      html ← chapter_0_html
-      case content (parseChapter html) of
-        SimpleChapterContent sections -> length sections `shouldBe` 21
-        ComplexChapterContent _ -> error "Got Subchapters but expected Sections"
+  it "finds the correct section names"   pending
 
-    it "finds the correct section names"
-      pending
-      
-      
-
-    it "finds the correct section content"
-      pending
+  it "finds the correct section content" pending
 
 --
 -- Helper Functions
