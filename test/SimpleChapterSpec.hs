6f6760b62a320e1df2b45299cb14408eb0b13585
Robb Shecter <robb@public.law>
2018-11-07 20:32:34 -0800

    another passing test

--- test/SimpleChapterSpec.hs
+++ test/SimpleChapterSpec.hs
@@ -39,9 +39,15 @@ spec = parallel $ describe "parseChapter" $ do
 
   it "finds the correct section content" $ do
     html <- chapter_0_html
-    let firstSection = head $ simpleChapterContent html
+    let firstSection = simpleChapterContent html !! 0
     Section.body firstSection
       `shouldBe` "<p class=SectBody><span class=\"Section\">0.010</span><span class=\"Empty\"> </span><span class=\"Leadline\">Scope.</span><span class=\"Empty\"> </span>This chapter provides definitions and declarations of legislative intent which apply to Nevada Revised Statutes as a whole.</p> <p class=\"SourceNote\"> (Added to NRS by <a href=\"../Statutes/59th/Stats197701.html#Stats197701page181\">1977, 181</a>)</p>"
+
+  it "finds the correct section content" $ do
+    html <- chapter_0_html
+    let secondSection = simpleChapterContent html !! 1
+    Section.body secondSection
+      `shouldBe` "<p class=SectBody><span class=\"Section\">0.020</span><span class=\"Empty\"> </span><span class=\"Leadline\">Severability.</span></p> <p class=\"SectBody\"> 1. If any provision of the Nevada Revised Statutes, or the application thereof to any person, thing or circumstance is held invalid, such invalidity shall not affect the provisions or application of NRS which can be given effect without the invalid provision or application, and to this end the provisions of NRS are declared to be severable.</p> <p class=\"SectBody\"> 2. The inclusion of an express declaration of severability in the enactment of any provision of NRS or the inclusion of any such provision in NRS, does not enhance the severability of the provision so treated or detract from the severability of any other provision of NRS.</p> <p class=\"SourceNote\"> (Added to NRS by <a href=\"../Statutes/59th/Stats197701.html#Stats197701page166\">1977, 166</a>)</p>"
 
 
   it "finds the correct section names" $ do
