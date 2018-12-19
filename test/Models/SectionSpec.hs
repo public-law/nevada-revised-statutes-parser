0943d80391cb8d7c992884e79837e1cb43ae1a63
Robb Shecter <robb@public.law>
2018-12-18 21:09:07 -0800

    failing test

--- test/Models/SectionSpec.hs
+++ test/Models/SectionSpec.hs
@@ -6,24 +6,27 @@ module Models.SectionSpec where
 import           BasicPrelude
 import           Test.Hspec
 
--- import           Models.Section                as Section
+import           Models.Section                as Section
 
 
 spec :: SpecWith ()
-spec = parallel $ describe "parseName" $ do
+spec = parallel $ do
+  describe "parseName" $ do
 
-  it "runs tests in a sub-module" $ do
-    let a = 1 :: Integer
-    let b = 2
-    a + b `shouldBe` 3
+    it "runs tests in a sub-module" $ do
+      let a = 1 :: Integer
+      let b = 2
+      a + b `shouldBe` 3
 
-  it "returns the name when it's simple" $ pending
+    it "returns the name when it's simple" $ do
+      let simpleName = "Definitions."
+      Section.parseName simpleName `shouldBe` simpleName
 
-  it "returns the name when there's a bracket notation" $ pending
+    it "returns the name when there's a bracket notation" $ pending
 
-  it "returns the bracket annotation when there is one" $ pending
+    it "returns the bracket annotation when there is one" $ pending
 
-  it "returns Nothing when there's no annotation" $ pending
+    it "returns Nothing when there's no annotation" $ pending
 
 
 --
