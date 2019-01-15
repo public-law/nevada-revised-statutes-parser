2858ffecb15c538016a81ae689fd9737a2dc5fb4
Robb Shecter <robb@public.law>
2019-01-15 01:17:40 -0800

    next try

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM ubuntu
+RUN apt-get update
+RUN apt-get install -y wget
+
 WORKDIR /app
-COPY . /app
-CMD ["app/download-and-parse.sh"]
+COPY app/download-and-parse.sh /app
+COPY .stack-work/install/x86_64-osx/lts-12.13/8.4.3/bin/nrs-parser-exe /app
+CMD ["download-and-parse.sh"]
--- app/download-and-parse.sh
+++ app/download-and-parse.sh
@@ -2,9 +2,8 @@
 
 # Download the NRS HTML files into /tmp/www.leg.state.nv.us
 cd /tmp
-rm -rf www.leg.state.nv.us
 wget --recursive --no-parent --no-verbose https://www.leg.state.nv.us/NRS/
 
 # Parse and produce JSON
 cd -
-.stack-work/install/x86_64-osx/lts-12.13/8.4.3/bin/nrs-parser-exe
+nrs-parser-exe
