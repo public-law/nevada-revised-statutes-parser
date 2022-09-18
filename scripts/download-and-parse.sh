c8bd072eabf3cc7fc3223a1f3c5e9622907ad7c3
Robb Shecter <robb@public.law>
2022-09-17 23:30:00 -0600

    save to s3 (#52)

--- scripts/download-and-parse.sh
+++ scripts/download-and-parse.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -x
+set -e
+
 # Download the NRS HTML files into /tmp/www.leg.state.nv.us
 cd /tmp
 wget --recursive --no-parent --no-verbose https://www.leg.state.nv.us/NRS/
 
 # Parse and produce JSON
 cd - > /dev/null  # "cd -" echoes the new dir to stdout.
-./nrs-parser-exe
+stack exec nrs-parser-exe > nevada-nrs.json
+
+# Upload result to S3
+s3cmd put nevada-nrs.json s3://publaw-scraping-results
