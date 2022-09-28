10eb94897abbb6701b94cec99db7fa3c5190a00f
Robb Shecter <robb@public.law>
2022-09-28 08:27:29 -0600

    refactor: eta reductions

--- src/ChapterFile.hs
+++ src/ChapterFile.hs
@@ -1,51 +1,54 @@
 module ChapterFile where
 
-import           BasicPrelude
-import qualified Data.Attoparsec.Text           ( Parser
-                                                , parseOnly
-                                                , takeText
-                                                , takeWhile
-                                                )
-import           Data.Char                      ( isAlpha
-                                                , isSpace
-                                                )
-import qualified Data.HashMap.Lazy             as HM
-import qualified Data.Text                     as T
-import           Text.HTML.TagSoup
-import           Text.Parser.Char
-import           Text.Printf
-import           Text.InterpolatedString.Perl6  ( qq )
-
-import           Config
-import           FileUtil                       ( RelativePath
-                                                , toRelativePath
-                                                )
-import           HtmlUtil                       ( Html
-                                                , titleText
-                                                , toText
-                                                )
-import qualified SimpleChapterFile              ( parseSectionsFromJustHtml
-                                                , parseSectionsFromHeadingGroup
-                                                , isSimpleSubChapter
-                                                )
-import           Models.Chapter                as Chapter
-import           Models.SubChapter             as SubChapter
-import           Models.SubSubChapter          as SubSubChapter
-import           Parsing                        ( ChapterData(..)
-                                                , TagList
-                                                )
-import           TextUtil                       ( normalizeWhiteSpace
-                                                , titleize
-                                                )
-
+import BasicPrelude
+import Config
+import qualified Data.Attoparsec.Text
+  ( Parser,
+    parseOnly,
+    takeText,
+    takeWhile,
+  )
+import Data.Char
+  ( isAlpha,
+    isSpace,
+  )
+import qualified Data.HashMap.Lazy as HM
+import qualified Data.Text as T
+import FileUtil
+  ( RelativePath,
+    toRelativePath,
+  )
+import HtmlUtil
+  ( Html,
+    titleText,
+    toText,
+  )
+import Models.Chapter as Chapter
+import Models.SubChapter as SubChapter
+import Models.SubSubChapter as SubSubChapter
+import Parsing
+  ( ChapterData (..),
+    TagList,
+  )
+import qualified SimpleChapterFile
+  ( isSimpleSubChapter,
+    parseSectionsFromHeadingGroup,
+    parseSectionsFromJustHtml,
+  )
+import Text.HTML.TagSoup
+import Text.InterpolatedString.Perl6 (qq)
+import Text.Parser.Char
+import Text.Printf
+import TextUtil
+  ( normalizeWhiteSpace,
+    titleize,
+  )
 
 type ChapterMap = HashMap RelativePath Html
 
 -- TODO: Use these to refactor.
 -- newtype SubChapterTOC = MakeSubChapterGroup TagList
 -- newtype SubSubChapterTOC = MakeSubSubChapterGroup TagList
-
-
 
 leadlineP :: String
 leadlineP = "<p class=COLeadline>"
@@ -59,152 +62,141 @@ heading4P = "<p class=COHead4>"
 horizontalRule :: String
 horizontalRule = "<p class=J-Dash"
 
-
-
 -- TODO: Refactor. Change this method by creating an EmptyChapter type.
 fillInEmptyChapter :: ChapterMap -> Chapter -> Chapter
 fillInEmptyChapter chapterMap emptyChapter =
-  let key       = chapterNumberToFilename (Chapter.number emptyChapter)
+  let key = chapterNumberToFilename (Chapter.number emptyChapter)
       maybeHtml = HM.lookup key chapterMap
-  in  if Chapter.number emptyChapter `notElem` chaptersToSkip
+   in if Chapter.number emptyChapter `notElem` chaptersToSkip
         then case maybeHtml of
           Just html -> case parseChapter html of
             Right aChapter -> aChapter
-            Left  message  -> error [qq| $message in $key |]
+            Left message -> error [qq| $message in $key |]
           Nothing -> error [qq| Chapter $key not found |]
         else emptyChapter
 
-
 chapterNumberToFilename :: Text -> RelativePath
 chapterNumberToFilename chapterNumber =
-  let format | isAlpha (T.last chapterNumber) = "%04s"
-             | otherwise                      = "%03s"
-  in  toRelativePath $ "NRS-" ++ printf format chapterNumber ++ ".html"
-
+  let format
+        | isAlpha (T.last chapterNumber) = "%04s"
+        | otherwise = "%03s"
+   in toRelativePath $ "NRS-" ++ printf format chapterNumber ++ ".html"
 
 parseChapter :: Html -> Either String Chapter
 parseChapter chapterHtml = do
-  let chapterData          = makeChapterData chapterHtml
-  rawTitle                <- titleText (headings chapterData)
+  let chapterData = makeChapterData chapterHtml
+  rawTitle <- titleText (headings chapterData)
   let (rawNumber, rawName) = parseChapterFileTitle rawTitle
   sectionsOrSubChapters <- chapterContent chapterData
 
-  return Chapter
-    { Chapter.name    = rawName
-    , Chapter.number  = rawNumber
-    , Chapter.url     = chapterUrlPrefix ++ rawNumber ++ ".html"
-    , Chapter.content = sectionsOrSubChapters
-    }
-
+  return
+    Chapter
+      { Chapter.name = rawName,
+        Chapter.number = rawNumber,
+        Chapter.url = chapterUrlPrefix ++ rawNumber ++ ".html",
+        Chapter.content = sectionsOrSubChapters
+      }
 
 makeChapterData :: Html -> ChapterData
 makeChapterData chapterHtml =
-  let fullPage    = parseTags $ toText chapterHtml
-      topHalf'    = topHalf fullPage
+  let fullPage = parseTags $ toText chapterHtml
+      topHalf' = topHalf fullPage
       bottomHalf' = bottomHalf fullPage
-  in  ChapterData
-        { headings      = topHalf'
-        , content       = bottomHalf'
-        , sectionGroups = findSectionGroups bottomHalf'
+   in ChapterData
+        { headings = topHalf',
+          content = bottomHalf',
+          sectionGroups = findSectionGroups bottomHalf'
         }
 
-
 findSectionGroups :: TagList -> [TagList]
-findSectionGroups contentHalf =
-  partitions (~== ("<span class=Section" :: String)) contentHalf
-
+findSectionGroups = partitions (~== ("<span class=Section" :: String))
 
 topHalf :: TagList -> TagList
-topHalf fullPage = takeWhile (~/= horizontalRule) fullPage
+topHalf = takeWhile (~/= horizontalRule)
 
 bottomHalf :: TagList -> TagList
-bottomHalf fullPage = dropWhile (~/= horizontalRule) fullPage
-
+bottomHalf = dropWhile (~/= horizontalRule)
 
 chapterContent :: ChapterData -> Either String ChapterContent
 chapterContent chapterData = do
   let groups = subChapterHeadingGroups chapterData
   foundSubChapters <- mapM (newSubChapter chapterData) groups
-  foundSections    <- SimpleChapterFile.parseSectionsFromJustHtml chapterData
+  foundSections <- SimpleChapterFile.parseSectionsFromJustHtml chapterData
   return $ case foundSubChapters of
     [] -> SimpleChapterContent foundSections
     xs -> ComplexChapterContent xs
 
-
 newSubChapter :: ChapterData -> TagList -> Either String SubChapter
 newSubChapter chapterData headingGroup = do
-  scs <- SimpleChapterFile.parseSectionsFromHeadingGroup chapterData
-                                                         headingGroup
+  scs <-
+    SimpleChapterFile.parseSectionsFromHeadingGroup
+      chapterData
+      headingGroup
   ssc <- parseSubSubChapters chapterData headingGroup
   let name' = subChapterNameFromGroup headingGroup
-  let children' = if SimpleChapterFile.isSimpleSubChapter headingGroup
-        then SubChapterSections scs
-        else SubSubChapters ssc
-  return SubChapter { SubChapter.name = name', SubChapter.children = children' }
-
+  let children' =
+        if SimpleChapterFile.isSimpleSubChapter headingGroup
+          then SubChapterSections scs
+          else SubSubChapters ssc
+  return SubChapter {SubChapter.name = name', SubChapter.children = children'}
 
 parseSubSubChapters :: ChapterData -> TagList -> Either String [SubSubChapter]
 parseSubSubChapters chapterData headingGroup =
   let parser = parseSubSubChapter chapterData
       groups = subSubChapterHeadingGroups headingGroup
-  in  mapM parser groups
-
+   in mapM parser groups
 
 parseSubSubChapter :: ChapterData -> TagList -> Either String SubSubChapter
 parseSubSubChapter chapterData subSubChapterHeadingGroup = do
   let name' = extractSubSubChapterName subSubChapterHeadingGroup
-  sections' <- SimpleChapterFile.parseSectionsFromHeadingGroup
-    chapterData
-    subSubChapterHeadingGroup
-  return SubSubChapter
-    { SubSubChapter.name     = name'
-    , SubSubChapter.sections = sections'
-    }
-
+  sections' <-
+    SimpleChapterFile.parseSectionsFromHeadingGroup
+      chapterData
+      subSubChapterHeadingGroup
+  return
+    SubSubChapter
+      { SubSubChapter.name = name',
+        SubSubChapter.sections = sections'
+      }
 
 extractSubSubChapterName :: TagList -> Text
 extractSubSubChapterName headingGroup =
   let linesOfText = lines $ innerText headingGroup
-  in  case linesOfText of
+   in case linesOfText of
         (x : _) -> normalizeWhiteSpace x
         _ ->
           error [qq|Couldn't parse sub sub chapter name from: $headingGroup|]
-
 
 subChapterNameFromGroup :: TagList -> Text
 subChapterNameFromGroup (_ : y : _) = titleize $ fromTagText y
 subChapterNameFromGroup tags =
   error [qq|Couldn't get a chapter name from the group: $tags|]
 
-
 -- A Sub Chapter heading with all of its following content.
 subChapterHeadingGroups :: ChapterData -> [TagList]
 subChapterHeadingGroups chapterData =
   partitions (~== heading2P) (headings chapterData)
 
-
 subSubChapterHeadingGroups :: TagList -> [TagList]
-subSubChapterHeadingGroups subChapterHeadingGroup =
-  partitions (~== heading4P) subChapterHeadingGroup
-
+subSubChapterHeadingGroups = partitions (~== heading4P)
 
 -- Input:  "NRS: CHAPTER 432B - PROTECTION OF CHILDREN FROM ABUSE AND NEGLECT"
 --    or:  "NRS: PRELIMINARY CHAPTER - GENERAL PROVISIONS"
 -- Output: ("432B", "Protection of Children from Abuse and Neglect")
 parseChapterFileTitle :: Text -> (Text, Text)
-parseChapterFileTitle input = if input == chapterZeroTitle
-  then ("0", "Preliminary Chapter – General Provisions")
-  else case (Data.Attoparsec.Text.parseOnly chapterTitleParser input) of
-    Left  e -> error [qq|Couldn't parse chapter file title $input $e|]
-    Right b -> b
-
+parseChapterFileTitle input =
+  if input == chapterZeroTitle
+    then ("0", "Preliminary Chapter – General Provisions")
+    else case Data.Attoparsec.Text.parseOnly chapterTitleParser input of
+      Left e -> error [qq|Couldn't parse chapter file title $input $e|]
+      Right b -> b
 
 -- Input:  "NRS: CHAPTER 432B - PROTECTION OF CHILDREN FROM ABUSE AND NEGLECT"
 -- Output: ("432B", "Protection of Children from Abuse and Neglect")
 chapterTitleParser :: Data.Attoparsec.Text.Parser (Text, Text)
 chapterTitleParser = do
-  _     <- string "NRS: CHAPTER "
-  num   <- Data.Attoparsec.Text.takeWhile (not . isSpace)
-  _     <- string " - "
+  _ <- string "NRS: CHAPTER "
+  num <- Data.Attoparsec.Text.takeWhile (not . isSpace)
+  _ <- string " - "
   title <- Data.Attoparsec.Text.takeText
   return (num, normalizeWhiteSpace $ titleize title)
--- src/IndexFile.hs
+++ src/IndexFile.hs
@@ -1,45 +1,39 @@
 module IndexFile where
 
-import           BasicPrelude
-import           Data.Attoparsec.Text    (parseOnly)
-import           Data.Function           ((&))
-import           Data.List.Split         (chunksOf, split, whenElt)
-import           Text.HTML.TagSoup       (fromAttrib, innerText, parseTags)
-import           Text.Parser.Char
-import           Text.Parser.Combinators
-import           Text.Parser.Token
-
-import           ChapterFile             (ChapterMap, fillInEmptyChapter)
-import           FileUtil                (toAbsoluteURL)
-import           HtmlUtil
-import           Models.Chapter          as Chapter
-import           Models.Title            as Title
-import           TextUtil                (normalizeWhiteSpace, titleize)
-
-
+import BasicPrelude
+import ChapterFile (ChapterMap, fillInEmptyChapter)
+import Data.Attoparsec.Text (parseOnly)
+import Data.Function ((&))
+import Data.List.Split (chunksOf, split, whenElt)
+import FileUtil (toAbsoluteURL)
+import HtmlUtil
+import Models.Chapter as Chapter
+import Models.Title as Title
+import Text.HTML.TagSoup (fromAttrib, innerText, parseTags)
+import Text.Parser.Char
+import Text.Parser.Combinators
+import Text.Parser.Token
+import TextUtil (normalizeWhiteSpace, titleize)
 
 parseTitlesAndChapters :: Html -> ChapterMap -> [Title]
 parseTitlesAndChapters indexHtml chapterMap =
   let emptyTitles = parseTitles indexHtml
    in case emptyTitles of
         Right ts -> map (fillInEmptyTitle chapterMap) ts
-        Left s   -> error s
-
+        Left s -> error s
 
 fillInEmptyTitle :: ChapterMap -> Title -> Title
 fillInEmptyTitle chapterMap emptyTitle =
   Title
-    { name = Title.name emptyTitle
-    , number = Title.number emptyTitle
-    , chapters = map (fillInEmptyChapter chapterMap) (chapters emptyTitle)
+    { name = Title.name emptyTitle,
+      number = Title.number emptyTitle,
+      chapters = map (fillInEmptyChapter chapterMap) (chapters emptyTitle)
     }
-
 
 parseTitles :: Html -> Either String [Title]
 parseTitles indexHtml = do
   content_rows <- contentRows indexHtml
   mapM newTitle (rowTuples content_rows)
-    
 
 contentRows :: Html -> Either String [Node]
 contentRows indexHtml = do
@@ -48,40 +42,37 @@ contentRows indexHtml = do
 
   return $ findAll "<tr>" table
 
-
 newTitle :: [[Node]] -> Either String Title
 newTitle tuple = do
   let titleRow = head (head tuple)
   first_b <- findFirst "<b>" titleRow
   let chapterRows = head $ tail tuple
   let parsedChapters = fmap newChapter chapterRows
-  let title = innerText $ first_b
+  let title = innerText first_b
   let (rawNumber, rawName) = parseRawTitle title
   return
     Title
-      { Title.name = titleize rawName
-      , Title.number = rawNumber
-      , chapters = rights parsedChapters
+      { Title.name = titleize rawName,
+        Title.number = rawNumber,
+        chapters = rights parsedChapters
       }
-
 
 newChapter :: Node -> Either String Chapter
 newChapter row = do
   first_anchor <- findFirst "<a>" row
   let columns = findAll "<td>" row
-  let name' = normalizeWhiteSpace $ innerText $ (last columns)
+  let name' = normalizeWhiteSpace $ innerText (last columns)
   let number' = head columns & innerText & normalizeWhiteSpace & words & last
   let chapter_url =
-        first_anchor & head & fromAttrib "href" &
-        toAbsoluteURL "https://www.leg.state.nv.us/NRS/"
+        first_anchor & head & fromAttrib "href"
+          & toAbsoluteURL "https://www.leg.state.nv.us/NRS/"
   return
     Chapter
-      { Chapter.name = name'
-      , Chapter.number = number'
-      , Chapter.url = chapter_url
-      , Chapter.content = ComplexChapterContent []
+      { Chapter.name = name',
+        Chapter.number = number',
+        Chapter.url = chapter_url,
+        Chapter.content = ComplexChapterContent []
       }
-
 
 -- Input:  "TITLE\n  1 — STATE JUDICIAL DEPARTMENT\n  \n \n "
 -- Output: (1, "STATE JUDICIAL DEPARTMENT")
@@ -89,16 +80,14 @@ parseRawTitle :: Text -> (Integer, Text)
 parseRawTitle input =
   let f = parseOnly p input
       p =
-        (,) <$> (textSymbol "TITLE" *> integer) <*>
-        (textSymbol "—" *> (fromString <$> many anyChar))
+        (,) <$> (textSymbol "TITLE" *> integer)
+          <*> (textSymbol "—" *> (fromString <$> many anyChar))
    in case f of
-        Left e  -> error e
+        Left e -> error e
         Right b -> b
-
 
 rowTuples :: [Node] -> [[[Node]]]
 rowTuples rows = split (whenElt isTitleRow) rows & tail & chunksOf 2
-
 
 isTitleRow :: Node -> Bool
 isTitleRow html = length (findAll "<td>" html) == 1
